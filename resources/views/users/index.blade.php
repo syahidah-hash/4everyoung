504859043711d1344f5e04cfad0dbc8aa7b70342
rahmrny12 <rahmatprayogo12@gmail.com>
2026-02-10 21:11:01 +0700

    fix: add user table styles

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,44 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <h1>Daftar Pengguna</h1>
+    <div>
+        <h1>Daftar Pengguna</h1>
 
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nama</th>
-                <th>Email</th>
-                <th>Nomor Member</th>
-                <th>Alamat</th>
-                <th>Telepon</th>
-            </tr>
-        </thead>
-
-        <tbody>
-           @foreach ($users as $user)
-    @forelse ($user->savings as $saving)
-        <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-            <td>{{ $user->membership_number }}</td>
-            <td>{{ $user->address }}</td>
-            <td>{{ $user->phone }}</td>
-            <td>{{ ucfirst($saving->saving_type) }}</td>
-            <td>Rp {{ number_format($saving->amount, 2, ',', '.') }}</td>
-        </tr>
-    @empty
-        <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-            <td>{{ $user->membership_number }}</td>
-            <td>{{ $user->address }}</td>
-            <td>{{ $user->phone }}</td>
-            <td colspan="2">Belum ada simpanan</td>
-        </tr>
-    @endforelse
-@endforeach
-
+        <div class="card shadow-sm">
+            <div class="card-body p-0">
+                <div class="table-responsive">
+                    <table class="table table-hover mb-0 align-middle">
+                        <thead class="table-light">
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama</th>
+                                <th>Email</th>
+                                <th>Nomor Member</th>
+                                <th>Alamat</th>
+                                <th>Telepon</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                @forelse ($user->savings as $saving)
+                                    <tr>
+                                        <td>{{ $user->id }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->membership_number }}</td>
+                                        <td>{{ $user->address }}</td>
+                                        <td>{{ $user->phone }}</td>
+                                        <td>{{ ucfirst($saving->saving_type) }}</td>
+                                        <td>Rp {{ number_format($saving->amount, 2, ',', '.') }}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td>{{ $user->id }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->membership_number }}</td>
+                                        <td>{{ $user->address }}</td>
+                                        <td>{{ $user->phone }}</td>
+                                        <td colspan="2">Belum ada simpanan</td>
+                                    </tr>
+                                @endforelse
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
